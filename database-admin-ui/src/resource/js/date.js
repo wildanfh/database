25d798eba0dffefff1607707489f2581f8d82bab
wildan <wildanf2005@gmail.com>
2022-08-31 09:41:00 +0700

    ok

--- database-admin-ui/src/resource/js/date.js
+++ database-admin-ui/src/resource/js/date.js
@@ -6,8 +6,8 @@ const month = date.getMonth();
 
 const bulan = ['January', 'February', 'Maret', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'Desember'];
 bulan.forEach(b => {
-  // console.log(`${b}, ${year}`)
-  document.querySelectorAll(".col.date").forEach(d => {
-    d.innerHTML = `${bulan[month]}, ${year}`;
-  });
+  console.log(`${b}, ${year}`)
+  // document.querySelectorAll(".col.date").forEach(d => {
+  //   d.innerHTML = `${bulan[month]}, ${year}`;
+  // });
 });
